00e34e66bb739eb0f5cdfdcb4048098ccbd39c19
UpscaleAcademy <UpscaleAcademy>
2017-07-03 09:08:29 +0300

    stage-8

--- app/src/components/Register.js
+++ app/src/components/Register.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
 import {Button, Icon} from 'react-materialize'
+import axios from 'axios';
 
 export default class Home extends Component {
 
+  registerSubmit = event => {
+    event.preventDefault();
+    let p, e;
+
+    [...this.form.elements].map(item => {
+      if(item.localName == 'input') {
+        if(item.type == 'email') e = item.value;
+        if(item.type == 'password') p = item.value;
+
+        item.value = '';
+      }
+    });
+
+    axios.post('server/requests/register.js', {})
+      .then((resolve, reject) => {
+        console.log(resolve, reject);
+      });
+
+  }
+  componentWillUnmount() {
+    this.email = null;
+    this.password = null;
+  }
   render() {
     return (
       <div className="register">
         <h1>Зарегестрироваться</h1>
-        <form className="register-form">
-          <input type="email" ref={email => this.email}/>
-          <input type="password" ref={password => this.password}/>
-          <Button waves='light'>Submit</Button>
+        <form className="register-form" ref={instance => this.form = instance}>
+          <input type="email" placeholder="Your email"/>
+          <input type="password" placeholder="Your password"/>
+          <Button waves='light' onClick={this.registerSubmit}>Submit</Button>
         </form>
       </div>
     )
